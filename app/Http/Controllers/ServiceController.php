4a59a7eb204fea13c84345421cad02bf0c5b0234
alexandremanso01 <alexandre_manso01@hotmail.com>
2021-06-09 14:29:01 -0300

    Atualizando avamoca

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -11,13 +11,13 @@ class ServiceController extends Controller
     private $api_server = 'https://ead.portalava.com.br/web_service';
     private $api_http_user = '1590e99c63d124e374345de71205ddb7c63a0b8d';
     private $api_http_pass = 'afb94979f63f3038b84344d7ac37febe39748167';
-    private $url_site = 'https://delta.noava.com.br/iesde/public';
+    private $url_site = 'https://avamoca.com.br/iesde/public';
 
     //CHAVE SIGMA
-    private $chave_acesso = '6eef8c3c11a667b3f65468b18e1b516f';
+    //private $chave_acesso = '6eef8c3c11a667b3f65468b18e1b516f';
 
     //CHAVE MOCA
-    //private $chave_acesso = '1a3e879fb888613f313d5e0ee22bca7f';
+    private $chave_acesso = '1a3e879fb888613f313d5e0ee22bca7f';
     
     private $chave_name = 'EAD-API-KEY';
     private $format = 'json';
